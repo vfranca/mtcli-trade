fcac6383cc3da6a62ccdf2811188b5459389be07
Valmir França <vfranca3@gmail.com>
2025-08-31 08:10:26 -0300

    Revisando make.bat

--- make.bat
+++ make.bat
@@ -1,41 +1,38 @@
 @echo off
 SET CMD=%1
 
-IF "%CMD%"=="test" (
+IF /i "%CMD%"=="test" (
 	echo Executando testes...
-	REM pytest tests -v
-	coverage run -m pytest
-	coverage report -m
+	poetry run pytest --cov=mtcli_trade tests/
 	goto end
 )
 
-IF "%CMD%"=="lint" (
+IF /i "%CMD%"=="lint" (
 	echo Executando linter ruff...
-	ruff check --fix .
+	poetry run ruff check --fix .
 	goto end
 )
 
 IF "%CMD%"=="format" (
 	echo Formatando o código com black...
-	black mtcli_trade tests
+	poetry run black mtcli_trade tests
     goto end
 )
 
 IF "%CMD%"=="check" (
 	echo Verificando com ruff e black...
-	ruff check .
-	black --check .
+	poetry run ruff check .
+	poetry run black --check .
 	goto end
 )
 
 if "%cmd%" == "typecheck" (
 	echo Verificando com mypy
-	mypy mtcli_trade tests
+	poetry run mypy mtcli_trade tests
 	goto end
 )
 
-
 echo Comando invalido: %CMD%
-echo "Uso: make [test | lint | format | check | typecheck]"
+echo Uso: make [test] [lint] [format] [check] [typecheck]
 
 :end
