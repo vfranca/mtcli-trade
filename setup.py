fa1944536fbf8f41fa382d9fea0752465638ca95
Valmir França <vfranca3@gmail.com>
2025-09-06 07:44:26 -0300

    Configurando para o RTD

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="mtcli-trade",
     version="1.2.0",
-    description="Plugin mtcli para gerenciamento de órdens e posições",
+    description="Plugin mtcli para gerenciamento de ordens",
     author="Valmir França da Silva",
     author_email="vfranca3@gmail.com",
-    packages=find_packages(),
+    packages=find_packages(include=["mtcli_trade", "mtcli_trade.*"]),
     install_requires=[
-        "mtcli>=1.19.4,<2.0.0",
+        "mtcli>=1.18.1,<2.0.0"
     ],
     entry_points={
         "mtcli.plugins": [
-            "trade = mtcli_trade.trade:trade",
-        ],
+"trade = mtcli_trade.trade:trade"
+        ]
     },
-    python_requires='>=3.10,<3.14',
 )
