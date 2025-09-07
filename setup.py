11baf70361e450d5cae5c338eddd9fb2fbbde5a1
Valmir França <vfranca3@gmail.com>
2025-09-07 12:01:24 -0300

    Editando dependencia do mtcli

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="mtcli-trade",
-    version="1.2.0",
+    version="1.2.1",
     description="Plugin mtcli para gerenciamento de ordens",
     author="Valmir França da Silva",
     author_email="vfranca3@gmail.com",
     packages=find_packages(include=["mtcli_trade", "mtcli_trade.*"]),
     install_requires=[
-        "mtcli>=1.18.1,<2.0.0"
+        "mtcli"
     ],
     entry_points={
         "mtcli.plugins": [
