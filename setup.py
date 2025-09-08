8ff613bc434386a964c3c470711c914998f96db8
Valmir França <vfranca3@gmail.com>
2025-09-08 03:52:03 -0300

    Alterando o build para poetry

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="mtcli-trade",
-    version="1.2.1",
-    description="Plugin mtcli para gerenciamento de ordens",
-    author="Valmir França da Silva",
-    author_email="vfranca3@gmail.com",
-    packages=find_packages(include=["mtcli_trade", "mtcli_trade.*"]),
-    install_requires=[
-        "mtcli"
-    ],
-    entry_points={
-        "mtcli.plugins": [
-"mtcli-trade = mtcli_trade.trade:trade"
-        ]
-    },
-)
+setup()
