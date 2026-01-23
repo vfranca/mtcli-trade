ff9bf9e07ad78179f43fa479bb60b5206726e178
Valmir França <vfranca3@gmail.com>
2026-01-23 03:15:41 -0300

    Revisando sell

--- mtcli_trade/clis/sell.py
+++ mtcli_trade/clis/sell.py
@@ -3,14 +3,14 @@ from ..controllers.sell_controller import SellController
 from ..conf import SYMBOL, LOT, SL, TP
 
 
-@click.command("sell")
-@click.option("--symbol", "-s", default=SYMBOL)
-@click.option("--lot", default=LOT, type=float)
-@click.option("-sl", default=SL, type=float)
-@click.option("-tp", default=TP, type=float)
-@click.option("--limit", is_flag=True)
-@click.option("--stop", is_flag=True)
-@click.option("--preco", type=float)
+@click.command()
+@click.option("--symbol", "-s", default=SYMBOL, show_default=True, help="Codigo do symbol.")
+@click.option("--lot", "-l", default=LOT, type=float, show_default=True, help="Quantidade de lotes.")
+@click.option("-sl", default=SL, type=float, show_default=True, help="Stop loss em pontos.")
+@click.option("-tp", default=TP, type=float, show_default=True, help="Take profit em pontos.")
+@click.option("--limit", "-lm", is_flag=True, help="Ordem limitada.")
+@click.option("--stop", "-s", is_flag=True, help="Ordem stop.")
+@click.option("--preco", "-p", type=float, help="Preco de entrada para ordem pendente.")
 def sell(symbol, lot, sl, tp, limit, stop, preco):
     """Envia ordem de VENDA (market | limit | stop)."""
     controller = SellController()
